36822e1e3747f400e7bea2297e75c648fa2bf13a
dirkraft <dirkraft@gmail.com>
2013-09-25 11:42:39 -0500

    raphael bootstrapped, bower is all i really want in dependency managements. require.js is a cluster

--- dm-ui/web/js/dm/dm-edit.js
+++ dm-ui/web/js/dm/dm-edit.js
@@ -1,7 +1,20 @@
-require.config({
-    baseUrl: 'js'
-});
+$(function () {
 
-require(["jquery"], function($) {
-    alert($('body'));
+    var width = 4096, height = 2048;
+
+    var paper = Raphael('graph', width, height);
+
+    var background = paper.rect(0, 0, width, height);
+    background.attr('stroke', '#ddd');
+    background.attr('fill', '#ddd');
+
+    // TODO move to graph util or some such
+    var step = 128;
+    for (var i = step; i < width; i += step) {
+        paper.path('M{0},{1}L{2},{3}'.format(i, 0, i, height)).attr('stroke', '#bbb');
+    }
+    for (var i = step; i < height; i += step) {
+        paper.path('M{0},{1}L{2},{3}'.format(0, i, width, i)).attr('stroke', '#bbb');
+    }
+
 });
